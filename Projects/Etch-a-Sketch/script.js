7c08bf02ab8e861dfa27f2ee4fc383e8dd60531d
aravind987 <arasu0511@gmail.com>
2022-01-18 22:24:49 -0500

    finished etch project

--- Projects/Etch-a-Sketch/script.js
+++ Projects/Etch-a-Sketch/script.js
@@ -6,14 +6,40 @@ function createDivs (numberOfSquares){
     
     for (i=0;i<((numberOfSquares*numberOfSquares));i++){
         const div = document.createElement('div');
-        div.classList.add('grid');
+        div.classList.add('cells');
         container.appendChild(div);
     }
 }
 
-function changeColour(){
-    
+function getGridSize(){
+    numberOfSquares = prompt("Enter grid size : ")
+    createDivs(numberOfSquares);
 }
 
-numberOfSquares = prompt("Enter grid size : ")
-createDivs(numberOfSquares);
+function eraseDivs(){
+    const elements = document.getElementsByClassName("cells");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function changeGridSize(){
+    eraseDivs();
+    getGridSize();
+}
+
+function resetGrid(){
+    const elements = document.getElementsByClassName('cells'); // get all elements
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.backgroundColor = "rgb(29, 56, 71)";
+	}
+}
+
+getGridSize();
+
+container.onmouseover = function (e){
+    e.target.style.backgroundColor = "rgb(127, 255, 212)";
+}
+
+gridSize.onclick = () => changeGridSize();
+reset.onclick = () => resetGrid();
