a03bc28850644b809ece273b791a888c614c06e8
aravind987 <arasu0511@gmail.com>
2022-01-24 22:29:21 -0500

    worked on script

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,3 +1,27 @@
 function add (a,b){
     return (a+b);
 }
+
+function subtract (a,b){
+    return (a-b);
+}
+
+function multiply (a,b){
+    return (a*b);
+}
+
+function divide (a,b){
+    return (a/b);
+}
+
+function operate (operator, a, b){
+    if (operator == 1){
+        add(a,b);
+    } else if (operator == 2){
+        subtract(a,b);
+    } else if (operator == 3){
+        multiply(a,b);
+    } else if (operator == 4){
+        divide(a,b);
+    }
+}
